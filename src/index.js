d2db2dc32f227613b1cfd92de485123ffcf9e1fd
ankitfront <ankitchakladar@gmail.com>
2025-10-06 10:42:22 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// require('dotenv').config();
+
+
+
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants"
+// import express from "express";
+
+
+// const app = express();
+// (async () => {
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//        app.on("error",()=>{
+//         console.log("Error connecting to MongoDB");
+//         throw error;
+//        })
+//          app.listen(process.env.PORT,()=>{
+//             console.log(`app is running on port ${process.env.PORT}`);
+//          })
+//     }
+//     catch(error){
+//         console.error("Error connecting to MongoDB:", error);
+//     }
+// })();
+import connectDB from "./db/index.js";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+});
+connectDB();
